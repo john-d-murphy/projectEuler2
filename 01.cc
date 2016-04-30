325e4ab3f5a75359d192a9965b13e448c59c8a4b
John Murphy <mr.proxxxy@gmail.com>
2016-04-30 18:11:31 -0400

    Added first problem. Easiest problem - fizzbuzz.

---
+++ 01.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+
+// As simple as you can do it - fizz buzz
+
+int main() {
+    int i = 0;
+    int sum = 0;
+    for (i = 3; i < 1000 ; i++) {
+      if (i % 3 == 0) {
+        sum += i;
+      } else if (i % 5 == 0) {
+        sum += i;
+      }
+    }
+    std::cout << sum << std::endl;
+    std::cin.get();
+    return 0;
+}
