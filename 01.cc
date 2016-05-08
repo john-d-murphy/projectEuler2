e9d1ab4a6e783a15ce638e7ca7b0b630b9d9cb3b
John Murphy <mr.proxxxy@gmail.com>
2016-05-08 11:59:47 -0400

    Updating for HackerRank Euler

--- 01.cc
+++ 01.cc
@@ -1,25 +1,48 @@
-#include <iostream>
 #include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
 /*
 Problem Description:
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-Find the sum of all the multiples of 3 or 5 below 1000. */
+Find the sum of all the multiples of 3 or 5 below 1000.
 
-// As simple as you can do it - fizz buzz
+HackerRank:
+Find the sum of all the multiples of 3 or 5 below N.
+
+Input Format
+First line contains T that denotes the number of test cases. This is followed by T lines, each containing an integer, N.
+
+Output Format
+For each test case, print an integer that denotes the sum of all the multiples of 3 or 5 below NN.
+
+Constraints
+1≤T≤10^5
+1≤N≤10^9
+
+*/
+
+long sumUnderValue(long difference, long valueToFindUnder) {
+  long numberOfTerms = (valueToFindUnder - 1)/difference;
+  long firstValue = difference;
+  long lastValue = numberOfTerms * difference;
+  long sumUnderValue = (numberOfTerms * (firstValue + lastValue))/2;
+  return sumUnderValue;
+}
 
 int main() {
-    int i = 0;
-    int sum = 0;
-    for (i = 3; i < 1000 ; i++) {
-      if (i % 3 == 0) {
-        sum += i;
-      } else if (i % 5 == 0) {
-        sum += i;
-      }
+  int testCases;
+  long n;
+  cin >> testCases;
+  for (int testCase = 0; testCase < testCases ; testCase++ ) {
+    while (cin >> n ) {
+      long sum = sumUnderValue(3,n) + sumUnderValue(5,n) - sumUnderValue(15,n);
+      cout << sum << endl;
     }
-    std::cout << sum << std::endl;
-    std::cin.get();
-    return 0;
+  }
+  return 0;
 }
