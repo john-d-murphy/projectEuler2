4b564cc51a3681de3c3095bb90688acc2e29f145
John Murphy <mr.proxxxy@gmail.com>
2016-05-08 12:39:31 -0400

    Updating for HackerRank

--- 03.cc
+++ 03.cc
@@ -12,24 +12,41 @@ Problem:
 
 The prime factors of 13195 are 5, 7, 13 and 29.
 
-What is the largest prime factor of the number 600851475143 ?
-----------------
-
-Simplest thing to do here is use trial division starting from 2.
+What is the largest prime factor of a given number N?
 
 */
 
-int main() {
-  long long numberToFactorize = 600851475143LL;
-  int trialNumber = 2;
-  cout << "Number to Factorize" << numberToFactorize << endl;
-  while (numberToFactorize > 1) {
+long largestPrimeFactor(long numberToFactorize) {
+  long trialNumber = 2;
+  double maxValue = sqrt(numberToFactorize);
+
+  /* we only need to divide up to the sqrt of the number to
+     factorize - we get to this point and we can make a
+     decision as to which number is the prime. */
+  while (numberToFactorize > 1 && trialNumber <= maxValue) {
       if ((numberToFactorize % trialNumber) == 0 ) {
         numberToFactorize = numberToFactorize/trialNumber;
-        cout << trialNumber << " divides evenly" << endl;
       } else {
         trialNumber++;
       }
   }
-  cin.get();
+
+  /* Now we have two numbers, one that's divisible by 2 and
+     one that's a prime number. We need to determine which is
+     which. */
+  if (numberToFactorize / 2 != 0) {
+    return numberToFactorize;
+  } else {
+    return trialNumber;
+  }
+}
+
+int main() {
+  int testCases;
+  long testValue;
+  cin >> testCases;
+  for (int testCase = 0; testCase < testCases ; testCase++ ) {
+    cin >> testValue;
+    cout << largestPrimeFactor(testValue) << endl;
+  }
 }
