5ef1cd8848dee680b6ba772d9b7c26920bc0391d
John Murphy <mr.proxxxy@gmail.com>
2016-06-09 18:32:14 -0400

    This one was a lot easier, though I needed to roll my own "BigInt" structure. Now I know that a vector of ints is like two orders of magnitude faster than a vector of strings!!
    Definitely don't need to do that in the future.

---
+++ 16.cc
@@ -0,0 +1,97 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+/*
+2^9 is 518 and the sum of its digits is 5 + 1 + 2 = 8.
+
+What is the sum of the digits of the number 2^N ?
+
+Input Format
+The first line contains an integer T , i.e., number of test cases.
+Next N lines will contain an integer N.
+
+Output Format
+Print the values corresponding to each test case.
+
+Constraints
+1 <= T <= 100
+1 <= N <= 10^4
+
+Sample Input
+3
+3
+4
+7
+Sample Output
+8
+7
+11 */
+
+vector<vector<int>> powersOfTwo(10000);
+vector<long> sumOfDigits(10000);
+
+vector<int> multiplyByTwo(vector<int> value) {
+  int overflow = 0;
+  vector<int> newValue;
+  int vectorSize = value.size();
+  for (int i = 0; i < vectorSize; i++) {
+    int placeValue = value[i];
+
+    // Get what this digit's double is
+    int multiple = placeValue * 2;
+
+    // Add it to our current working overflow
+    int charValue = overflow + multiple;
+    int digit = charValue % 10;
+
+    // Get new overflow value
+    overflow = (charValue - charValue % 10)/10;
+
+    // Prepend new digit
+    newValue.push_back(digit);
+  }
+
+  if (overflow > 0) {
+    newValue.push_back(overflow);
+  }
+
+  newValue = newValue;
+
+  return newValue;
+}
+
+long sumDigits(int powerOfTwo) {
+  vector<int> value = powersOfTwo[powerOfTwo];
+  long sum = 0;
+  for (int i = 0; i < value.size(); i++) {
+    long placeValue = value[i];
+    sum += placeValue;
+  }
+  return sum;
+}
+
+int main() {
+  // Precompute
+
+  powersOfTwo[0].push_back(1);
+  for (int i = 1 ; i <= 10000; i++) {
+    vector<int> value = powersOfTwo[i-1];
+    vector<int> result = multiplyByTwo(value);
+    powersOfTwo[i] = result;
+    long sum = sumDigits(i);
+    sumOfDigits[i] = sum;
+  }
+  // Run TestCases
+  int testCases;
+  cin >> testCases;
+  for (int testCase = 0; testCase < testCases ; testCase++ ) {
+    int testValue;
+    cin >> testValue;
+    cout << sumOfDigits[testValue] << endl;
+  }
+}
