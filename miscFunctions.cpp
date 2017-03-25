3cb79a4974c4cb034c5d6ab94893f6b22ee95baf
John Murphy <mr.proxxxy@gmail.com>
2017-03-25 15:51:44 -0400

    Updating formatting

--- miscFunctions.cpp
+++ miscFunctions.cpp
@@ -1,22 +1,20 @@
 
 // Recursive Euclid's algorithm -> we'll see if this is actually efficient
 int greatestCommonDivisor(int a, int b) {
-    int gcd = (b == 0) ? a : greatestCommonDivisor(b, a % b);
-    return gcd;
+  int gcd = (b == 0) ? a : greatestCommonDivisor(b, a % b);
+  return gcd;
 }
 
-int leastCommonMultiple(int a, int b) {
-    return (a/greatestCommonDivisor(a,b)) * b;
-}
+int leastCommonMultiple(int a, int b) { return (a / greatestCommonDivisor(a, b)) * b; }
 
 void sieveOfEratosthenes(int maxInt, bool primes[]) {
-  for (int i = 0 ; i < maxInt ; i++) {
+  for (int i = 0; i < maxInt; i++) {
     primes[i] = true;
   }
 
   for (int i = 2; i <= maxInt; i++) {
     if (primes[i] == true) {
-      for (int j = i+i; j <= maxInt; j+=i) {
+      for (int j = i + i; j <= maxInt; j += i) {
         primes[j] = false;
       }
     }
